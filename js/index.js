79caf9bccdb0820a979e08ac9de061244860cfc8
hoodley-rootz <hadleyreetz@gmail.com>
2023-03-26 13:28:19 -0500

    First Commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+$(".campToCrag").hover(function(){
+    $(this).animate({
+        height: "600px"
+    }, 800);
+    }, function(){
+    $(this).animate({
+       height: "420px" 
+  },800);
+});
+
+$(".DOE").hover(function(){
+    $(this).animate({
+        height: "600px"
+    }, 800);
+    }, function(){
+    $(this).animate({
+       height: "420px" 
+  },800);
+});
+
+$(".CCUS").hover(function(){
+    $(this).animate({
+        height: "600px"
+    }, 800);
+    }, function(){
+    $(this).animate({
+       height: "420px" 
+  },800);
+});
+
+console.log('hellow workld');
